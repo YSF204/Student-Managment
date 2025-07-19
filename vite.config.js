9f57861df3fbd33d163521d959b14d78d4ee96c3
Yousef AL-Bakri <yousef204b@gmail.com>
2025-07-19 19:17:24 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/building-a-student-management-app-with-react-YSF204/",
+  base: "/Student-Managment/",
   plugins: [react()],
 });
